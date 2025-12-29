b60312d0a069aefd3ec58c926bdc6a67e5332465
Brandon Brenes Arias <145218565+esveny@users.noreply.github.com>
2025-12-29 11:13:35 -0600

    Agregar adaptador Vercel para endpoints API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
   site: 'https://tallerindustrialbrenes.com',
   integrations: [
     tailwind({
