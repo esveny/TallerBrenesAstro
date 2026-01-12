95c3de026094d32c542c82c6fa7d230d3cad2324
Brandon Brenes Arias <145218565+esveny@users.noreply.github.com>
2026-01-12 14:19:58 -0600

    Fix: Usar serverless adapter con output server

--- src/pages/api/verify-recaptcha.ts
+++ src/pages/api/verify-recaptcha.ts
@@ -1,7 +1,5 @@
 import type { APIRoute } from 'astro';
 
-export const prerender = false;
-
 export const POST: APIRoute = async ({ request }) => {
   try {
     const { token } = await request.json();
